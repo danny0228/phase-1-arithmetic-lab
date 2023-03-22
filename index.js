fe0597c553a00a37a4e405e47ce8f285b86490ae
Daniel Tuiran <danny0228@hotmail.com>
2023-03-22 17:41:14 -0400

    arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-
 // Write your code here
+const num1 = 2;
+const num2 = 31;
+let multiply = (num1 * num2);
+
+const random = parseInt(Math.random() * 100);
+
+const num3 = 19;
+const num4 = 5;
+const mod = (num3 % num4);
+
+const mySet = [2, 4, 6, 8, 20];
+let max = Math.max(...mySet);
+
